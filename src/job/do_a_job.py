09612c8efc23971ed8f6f00f2ce0e75ed8ae9251
Bob Gregory <bob@codefiend.co.uk>
2023-12-13 14:32:17 +0000

    create repro

---
+++ src/job/do_a_job.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from src.shared_code import foo
+
+print("hello world")
+foo.a_very_important_function()
---
+++ src/shared_code/foo.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+def a_very_important_function():
+    print("hello, you")
